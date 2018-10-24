2f512b4b4805a17480362e4f6b7953bbd6dad40b
1ByteMemory <32960450+1ByteMemory@users.noreply.github.com>
2018-10-24 17:31:03 +0100

    Add files via upload

---
+++ 2D_Platformer_Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    CharacterController Cc;
+    public float playerSpeed = 5.0f;
+    public float playerMass = 0.5f;
+
+    private BoxCollider isGrounded;
+
+	// Use this for initialization
+	void Start () {
+        isGrounded = GetComponentInChildren<BoxCollider>();
+        Cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float h = Input.GetAxis("Horizontal");
+        
+        Cc.Move(new Vector3(h * playerSpeed * Time.deltaTime, 0, 0));
+	}
+
+    public void Ground(bool isGrounded)
+    {
+        if (isGrounded == false)
+        {
+
+        }
+    }
+}
