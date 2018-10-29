f5bc364be56fb9d9082ab7a51025c03646ef2a20
1ByteMemory <32960450+1ByteMemory@users.noreply.github.com>
2018-10-29 15:05:02 +0000

    Jumping kinda works

--- 2D_Platformer_Game/Assets/Scripts/PlayerMovement.cs
+++ 2D_Platformer_Game/Assets/Scripts/PlayerMovement.cs
@@ -6,29 +6,39 @@ public class PlayerMovement : MonoBehaviour {
 
     CharacterController Cc;
     public float playerSpeed = 5.0f;
-    public float playerMass = 0.5f;
+    public float jumpSpees = 5.0f;
+    public float gravity = 20.0f;
+    public float maxJumpHieght = 2.0f;
 
-    private BoxCollider isGrounded;
+    private float airTime;
 
 	// Use this for initialization
 	void Start () {
-        isGrounded = GetComponentInChildren<BoxCollider>();
         Cc = GetComponent<CharacterController>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         float h = Input.GetAxis("Horizontal");
-        
-        Cc.Move(new Vector3(h * playerSpeed * Time.deltaTime, 0, 0));
-	}
+        float y = Input.GetAxis("Jump");
 
-    public void Ground(bool isGrounded)
-    {
-        if (isGrounded == false)
+        Cc.Move(new Vector3(h * playerSpeed * Time.deltaTime, y * jumpSpees * Time.deltaTime, 0));
+
+        if (!Cc.isGrounded)
         {
+            airTime += Time.deltaTime * 2;
+        }
 
+        if (!Cc.isGrounded && y == 0 && airTime >= maxJumpHieght)
+        {
+            Cc.Move(new Vector3(h * playerSpeed * Time.deltaTime, gravity * Time.deltaTime * -1, 0));
+        }
+
+        if (Cc.isGrounded)
+        {
+            airTime = 0.0f;
         }
     }
 }
