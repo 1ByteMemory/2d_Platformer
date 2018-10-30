b5683a2ab7f55aefab8deb531cd375f1bd943cdf
Dan <dan.jones8902@gmail.com>
2018-10-30 16:11:21 +0000

    Editing Jump mechanics. Removed Grounded and Test script.

--- 2D_Platformer_Game/Assets/Scripts/PlayerMovement.cs
+++ 2D_Platformer_Game/Assets/Scripts/PlayerMovement.cs
@@ -5,40 +5,43 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     CharacterController Cc;
-    public float playerSpeed = 5.0f;
-    public float jumpSpees = 5.0f;
-    public float gravity = 20.0f;
-    public float maxJumpHieght = 2.0f;
+    public float playerSpeed = 5f;
+    public float jumpSpees = 5f;
+    public float gravity = 20f;
+    public float maxJumpHieght = 2f;
 
-    private float airTime;
+    private float airTime = 0f;
+    float y;
+
 
 	// Use this for initialization
 	void Start () {
         Cc = GetComponent<CharacterController>();
+        airTime = maxJumpHieght + 1f;
+        y = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         float h = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Jump");
 
-        Cc.Move(new Vector3(h * playerSpeed * Time.deltaTime, y * jumpSpees * Time.deltaTime, 0));
 
         if (!Cc.isGrounded)
         {
-            airTime += Time.deltaTime * 2;
-        }
-
-        if (!Cc.isGrounded && y == 0 && airTime >= maxJumpHieght)
-        {
-            Cc.Move(new Vector3(h * playerSpeed * Time.deltaTime, gravity * Time.deltaTime * -1, 0));
+            airTime += 1 * Time.deltaTime;
+            if (airTime >= maxJumpHieght)
+            {
+                y = 0f;
+                Cc.Move(new Vector3(h * playerSpeed * Time.deltaTime, -gravity * Time.deltaTime, 0));
+            }
         }
 
         if (Cc.isGrounded)
         {
-            airTime = 0.0f;
+            airTime = 0f;
+            y = Input.GetAxis("Jump");
+            Cc.Move(new Vector3(h * playerSpeed * Time.deltaTime, y * jumpSpees * Time.deltaTime, 0));
         }
     }
 }
