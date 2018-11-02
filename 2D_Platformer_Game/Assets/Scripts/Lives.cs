4e46c1ffd18b6f5ae0178b235c0997796f8d01a3
Dan <dan.jones8902@gmail.com>
2018-11-02 16:15:40 +0000

    Removed unesesery Mats. Expanded Level. Working on death Script.

---
+++ 2D_Platformer_Game/Assets/Scripts/Lives.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour {
+    
+    public int playerLives = 3;
+
+    public void PlayerLives (int amount)
+    {
+        playerLives -= amount;
+
+        // Death Function
+        if (playerLives <= 0)
+        {
+            // restart the scene.
+        }
+    }
+
+}
---
+++ 2D_Platformer_Game/Assets/Scripts/OfDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfDeath : MonoBehaviour {
+
+    public int damage = 1;
+
+    private Lives script;
+
+    private void Start()
+    {
+        script = FindObjectOfType<Lives>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            script.PlayerLives(damage);
+        }
+    }
+}
