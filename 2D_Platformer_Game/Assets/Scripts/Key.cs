f11565c3eb707e3234885c97858767dad414acd9
1ByteMemory <32960450+1ByteMemory@users.noreply.github.com>
2018-11-06 13:41:42 +0000

    Moving Platform

---
+++ 2D_Platformer_Game/Assets/Scripts/Key.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+    public GameObject door;
+
+    private bool called = false;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Destroy(gameObject);
+        UnlockDoor(door);
+    }
+
+    public void UnlockDoor (GameObject door)
+    {
+        if (!called)
+        {
+            Destroy(door);
+            called = true;
+        }
+    }
+}
---
+++ 2D_Platformer_Game/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            player.transform.SetParent(gameObject.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            player.transform.SetParent(null);
+        }
+    }
+}
