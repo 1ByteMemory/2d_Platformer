279d2b344af7a5cf51abc8544d5eff54d57252cb
Dan <dan.jones8902@gmail.com>
2018-11-06 18:43:36 +0000

    Lever the release a stalactite that crashes through floor.

---
+++ 2D_Platformer_Game/Assets/Scripts/BrackPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrackPlatform : MonoBehaviour {
+
+    Rigidbody rb;
+    public GameObject breaker;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject == breaker)
+        {
+            rb.useGravity = true;
+        }
+    }
+}
---
+++ 2D_Platformer_Game/Assets/Scripts/Lever.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour {
+
+    public GameObject fallingObject;
+
+    Animator anim;
+    bool played = false;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (played == false)
+            {
+                played = true;
+                anim.SetBool("On", true);
+            }
+        }
+    }
+
+    public void Release ()
+    {
+        fallingObject.GetComponent<Rigidbody>().useGravity = true;
+    }
+}
