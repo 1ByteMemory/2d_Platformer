36b924b3ee0d279ddff44a883baa56c10957def4
Dan <dan.jones8902@gmail.com>
2018-11-08 00:22:45 +0000

    Level 2

---
+++ 2D_Platformer_Game/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.buildIndex + 1);
+        }
+    }
+}
